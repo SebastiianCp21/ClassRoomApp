1448e2084652d078a89b7beb5185d677fcf714a3
SebastiianCp21 <sebastiancruz2534@gmail.com>
2025-03-21 18:57:58 -0500

    feat se crea proyecto base para el api

---
+++ ApiClassRoom/src/main/java/com/example/ApiClassRoom/ApiClassRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiClassRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClassRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClassRoomApplication.class, args);
+	}
+
+}
